a168b4800acebd4ff13286e312a09974a2170e10
Ethan Steinberg <esteinberg@prealizehealth.com>
2024-01-03 22:34:18 +0000

    Finish flat

--- README.md
+++ README.md
@@ -6,19 +6,19 @@ This package library currently supports:
 
 - MIMIC-IV
 - OMOP v5
-- MEDS CSV, a simple CSV format
+- MEDS FLAT, a flat version of MEDS
 
 ## MIMIC-IV
 
 In order to run the MIMIC-IV ETL, simply run the following command:
 
-`meds_etl_mimic mimiciv mimiciv-esds` where mimiciv is a download of MIMIC-IV and mimiciv-esds will be the destination path for the dataset.
+`meds_etl_mimic mimiciv mimiciv_meds` where mimiciv is a download of MIMIC-IV and mimiciv-esds will be the destination path for the dataset.
 
 ## OMOP
 
 In order to run the MIMIC-IV ETL, simply run the following command:
 
-`meds_etl_omop omop omop-esds` where omop is a folder containing csv files (optionally gzipped) for an OMOP dataset. Each table should either be a csv file with the table name (such as person.csv) or a folder with the table name containing csv files.
+`meds_etl_omop omop omop_meds` where omop is a folder containing csv files (optionally gzipped) for an OMOP dataset. Each table should either be a csv file with the table name (such as person.csv) or a folder with the table name containing csv files.
 
 ## Unit tests
 
@@ -39,27 +39,27 @@ wget -r -N -c --no-host-directories --cut-dirs=1 -np -P tests/data https://physi
 
 ## MEDS Flat
 
-The MEDS schema can be a bit tricky to directly ETL into as it is a nested parquet schema and nested schemas are not supported by many pieces of software. In additional, the global join of combining all of a patient's data into a single value can be difficult to implement.
+The MEDS schema can be a bit tricky to use as it is a nested parquet schema and nested schemas are not as widely supported as flat schemas. For example, it's not possible to represent a nested schema with CSV files. In additional, the implicit global join within MEDS in order to combine all of a patient's data into a single value can be difficult to implement.
 
-In order to make things simpler for users, we have a special MEDS Flat schema, and ETLs back and forth between MEDS and MEDS Flat.
+In order to make things simpler for users, this package provides a special MEDS Flat schema and ETLs that transform between MEDS Flat and MEDS.
 
-This MEDS CSV schema is 1-1 compatible with MEDS. It is a simple tabular schema that has four core columns: patient_id, time, code, value. These columns correspond to the core ESDS columns, with the one exception that all the value columns have been combined into a single one.
+This MEDS CSV schema is 1-1 compatible with MEDS. It is a simple tabular schema that has four core columns: patient_id, time, code, value. These columns correspond to the core ESDS columns, with the one exception that all the value columns have been combined into a single one. Additional columns can be present, which correspond to event metadata.
 
-patient_id is a 64-bit integer, time is an ISO 8601 datetime, and code is a string. value is either a number, an ISO 8601 datetime, or arbitrary text. 
+For MEDS Flat, we support both CSV and Parquet input files for transforming into MEDS.
 
-These columns get converted into the standard ESDS columns, with value converted to either numeric_value, datetime_value or text_value as necessary. 
+In order to convert a MEDS Flat dataset into MEDS, simple run the following command:
 
-The simple CSV format also supports arbitrary additional columns, that get converted into a metadata struct.
+`meds_etl_flat flat flat_meds` where flat is a folder containing MEDS Flat data and `flat_meds` is the target folder to store the results in.
 
 For example, the following CSV would be converted into the following MEDS patient:
 
 Input CSV:
 ```
-patient_id,time,code,value,arbitrary_metadata_column
-100,1990-11-30,Birth/Birth,,a string
-100,1990-11-30,Gender/Gender,Male,another string
-100,1990-11-30,Labs/SystolicBloodPressure,100,
-100,1990-12-28,ICD10CM/E11.4,,anything
+patient_id,time,code,text_value,numeric_value,datetime_value,arbitrary_metadata_column
+100,1990-11-30,Birth/Birth,,,,a string
+100,1990-11-30,Gender/Gender,Male,,,another string
+100,1990-11-30,Labs/SystolicBloodPressure,,100,,
+100,1990-12-28,ICD10CM/E11.4,,,,anything
 ```
 
 Output MEDS Patient:
@@ -78,6 +78,6 @@ Output MEDS Patient:
     {
       'time': 1990-12-28,
       'measurements': [{'code': 'ICD10CM/E11.4', 'metadata': {'arbitrary_metadata_column': 'anything'}}],
-    }, 
+    },
   ]
 }
--- src/meds_etl/flat.py
+++ src/meds_etl/flat.py
@@ -1,74 +1,88 @@
+from __future__ import annotations
+
 import argparse
-import json
+import csv
+import functools
+import gzip
 import multiprocessing
 import os
-import pickle
+import pathlib
 import random
 import shutil
 import subprocess
 import tempfile
-import functools
-import pathlib
+from typing import Iterable, List, Literal, Set, TextIO, cast
 
-import jsonschema
 import meds
 import polars as pl
+import pyarrow as pa
 import pyarrow.parquet as pq
 
-import meds_etl
+mp = multiprocessing.get_context("forkserver")
 
-from typing import Literal, Iterable, TextIO
+Format = Literal["csv", "parquet", "compressed_csv"]
 
-Format = Literal['csv', 'parquet', 'compressed_csv']
 
 def convert_file_to_flat(source_file: str, *, target_flat_data_path: str, format: Format, time_format: str) -> None:
     table = pl.scan_parquet(source_file)
-    
-    table = table.explode('events')
-    table = table.unnest('events')
 
-    table = table.explode('measurements')
-    table = table.unnest('measurements')
+    table = table.explode("events")
+    table = table.unnest("events")
 
-    if table.schema['metadata'] == pl.Null():
-        table = table.drop('metadata')
+    table = table.explode("measurements")
+    table = table.unnest("measurements")
+
+    if table.schema["metadata"] == pl.Null():
+        table = table.drop("metadata")
     else:
-        table = table.unnest('metadata')
+        table = table.unnest("metadata")
 
-    if format == 'parquet':
-        table.sink_parquet(os.path.join(target_flat_data_path, pathlib.Path(source_file).with_suffix('.parquet').name))
-    elif format.endswith('csv'):
+    if format == "parquet":
+        table.sink_parquet(os.path.join(target_flat_data_path, pathlib.Path(source_file).with_suffix(".parquet").name))
+    elif format.endswith("csv"):
         table = table.with_columns(
-            time=pl.col('time').dt.strftime(time_format),
-            datetime_value=pl.col('datetime_value').dt.strftime(time_format),
+            time=pl.col("time").dt.strftime(time_format),
+            datetime_value=pl.col("datetime_value").dt.strftime(time_format),
         )
-        target = os.path.join(target_flat_data_path, pathlib.Path(source_file).with_suffix('.csv').name)
+        target = os.path.join(target_flat_data_path, pathlib.Path(source_file).with_suffix(".csv").name)
         table.sink_csv(target)
-        if format == 'compressed_csv':
-            subprocess.run(["gzip", '-f', target])
+        if format == "compressed_csv":
+            subprocess.run(["gzip", "-f", target])
 
-def convert_meds_to_flat(source_meds_path: str, target_flat_path: str, num_proc: int = 1, format: Format = "compressed_csv", time_format: str = "%Y-%m-%d %H:%M:%S%.f") -> None:
-#    os.mkdir(target_flat_path)
 
-    source_meds_data_path = os.path.join(source_meds_path, 'data')
+def convert_meds_to_flat(
+    source_meds_path: str,
+    target_flat_path: str,
+    num_proc: int = 1,
+    format: Format = "compressed_csv",
+    time_format: str = "%Y-%m-%d %H:%M:%S%.f",
+) -> None:
+    os.mkdir(target_flat_path)
+
+    source_meds_data_path = os.path.join(source_meds_path, "data")
     source_files = os.listdir(source_meds_data_path)
     tasks = [os.path.join(source_meds_data_path, source_file) for source_file in source_files]
-    
-    shutil.copyfile(os.path.join(source_meds_path, 'metadata.json'), os.path.join(target_flat_path, 'metadata.json'))
-    
+
+    shutil.copyfile(os.path.join(source_meds_path, "metadata.json"), os.path.join(target_flat_path, "metadata.json"))
+
     target_flat_data_path = os.path.join(target_flat_path, "flat_data")
-#    os.mkdir(target_flat_data_path)
+    os.mkdir(target_flat_data_path)
 
     converter = functools.partial(
-        convert_file_to_flat, 
-        target_flat_data_path=target_flat_data_path, 
-        format=format, 
+        convert_file_to_flat,
+        target_flat_data_path=target_flat_data_path,
+        format=format,
         time_format=time_format,
     )
 
-    with multiprocessing.Pool(processes=num_proc) as pool:
-        for _ in pool.imap_unordered(converter, tasks):
-            pass
+    if num_proc != 1:
+        with mp.Pool(processes=num_proc) as pool:
+            for _ in pool.imap_unordered(converter, tasks):
+                pass
+    else:
+        for task in tasks:
+            converter(task)
+
 
 def convert_meds_to_flat_main():
     parser = argparse.ArgumentParser(prog="meds_reverse_etl_flat", description="Performs an ETL from MEDS to MEDS Flat")
@@ -79,15 +93,52 @@ def convert_meds_to_flat_main():
     args = parser.parse_args()
     convert_meds_to_flat(**vars(args))
 
+
 def load_file(decompressed_dir: str, fname: str) -> TextIO:
     if fname.endswith(".gz"):
         file = tempfile.NamedTemporaryFile(dir=decompressed_dir)
         subprocess.run(["gunzip", "-c", fname], stdout=file)
-        return file
+        return cast(TextIO, file)
     else:
         return open(fname)
 
-def process_parquet_file(event_file: str, *, temp_dir: str, num_shards: int):
+
+def get_csv_columns(event_file: str) -> Set[str]:
+    if event_file.endswith(".gz"):
+        with gzip.open(event_file, "rt") as f:
+            reader = csv.reader(f)
+            return set(next(reader))
+    else:
+        with open(event_file, "r") as f:
+            reader = csv.reader(f)
+            return set(next(reader))
+
+
+def get_parquet_columns(event_file: str) -> Set[str]:
+    schema = pq.read_schema(event_file)
+    return set(schema.names)
+
+
+def transform_metadata(d, metadata_columns):
+    cols = []
+    for column in metadata_columns:
+        val = d.get(column, pl.lit(None, dtype=pl.Utf8))
+        cols.append(val.alias(column))
+
+    return pl.struct(cols)
+
+
+def verify_shard(shard, event_file):
+    for important_column in ("patient_id", "time", "code"):
+        rows_with_invalid_code = shard.filter(pl.col(important_column).is_null())
+        if len(rows_with_invalid_code) != 0:
+            print("Have rows with invalid " + important_column + " " + event_file)
+            for row in rows_with_invalid_code:
+                print(row)
+            raise ValueError("Cannot have rows with invalid " + important_column + " " + event_file)
+
+
+def process_parquet_file(event_file: str, *, temp_dir: str, num_shards: int, metadata_columns: List[str]):
     print("Working on ", event_file)
 
     table = pl.scan_parquet(event_file)
@@ -96,7 +147,7 @@ def process_parquet_file(event_file: str, *, temp_dir: str, num_shards: int):
 
     patient_id = pl.col("patient_id")
     time = pl.col("time")
-    
+
     code = pl.col("code")
 
     datetime_value = pl.col("datetime_value")
@@ -112,10 +163,7 @@ def process_parquet_file(event_file: str, *, temp_dir: str, num_shards: int):
             continue
         metadata[colname] = pl.col(colname)
 
-    def transform_metadata(d):
-        return pl.struct([v.alias(k) for k, v in d.items()])
-
-    metadata = transform_metadata(metadata)
+    metadata = transform_metadata(metadata, metadata_columns)
 
     event_data = (
         table.select(
@@ -133,15 +181,22 @@ def process_parquet_file(event_file: str, *, temp_dir: str, num_shards: int):
     )
 
     for shard_index, shard in event_data.items():
-        cleaned_event_file = os.path.basename(event_file).split('.')[0]
-        fname = os.path.join(
-            temp_dir, str(shard_index), f'{cleaned_event_file}.parquet'
-        )
+        verify_shard(shard, event_file)
+
+        cleaned_event_file = os.path.basename(event_file).split(".")[0]
+        fname = os.path.join(temp_dir, str(shard_index), f"{cleaned_event_file}.parquet")
         shard.write_parquet(fname, compression="uncompressed")
 
 
-
-def process_csv_file(event_file: str, *, decompressed_dir: str, temp_dir: str, num_shards: int, time_formats: Iterable[str]):
+def process_csv_file(
+    event_file: str,
+    *,
+    decompressed_dir: str,
+    temp_dir: str,
+    num_shards: int,
+    time_formats: Iterable[str],
+    metadata_columns: List[str],
+):
     print("Working on ", event_file)
 
     with load_file(decompressed_dir, event_file) as temp_f:
@@ -162,17 +217,14 @@ def process_csv_file(event_file: str, *, decompressed_dir: str, temp_dir: str, n
 
             patient_id = pl.col("patient_id").cast(pl.Int64)
             time = pl.col("time")
-            
-            
-            time = pl.coalesce([
-                time.str.to_datetime(time_format, strict=False) for time_format in time_formats
-            ])
+
+            time = pl.coalesce([time.str.to_datetime(time_format, strict=False) for time_format in time_formats])
 
             code = pl.col("code")
 
-            datetime_value = pl.coalesce([
-                pl.col("datetime_value").str.to_datetime(time_format, strict=False) for time_format in time_formats
-            ])
+            datetime_value = pl.coalesce(
+                [pl.col("datetime_value").str.to_datetime(time_format, strict=False) for time_format in time_formats]
+            )
 
             numeric_value = pl.col("numeric_value").cast(pl.Float32, strict=False)
 
@@ -185,10 +237,7 @@ def process_csv_file(event_file: str, *, decompressed_dir: str, temp_dir: str, n
                     continue
                 metadata[colname] = pl.col(colname)
 
-            def transform_metadata(d):
-                return pl.struct([v.alias(k) for k, v in d.items()])
-
-            metadata = transform_metadata(metadata)
+            metadata = transform_metadata(metadata, metadata_columns)
 
             batch = batch.filter(code.is_not_null())
 
@@ -208,14 +257,19 @@ def process_csv_file(event_file: str, *, decompressed_dir: str, temp_dir: str, n
             )
 
             for shard_index, shard in event_data.items():
-                cleaned_event_file = os.path.basename(event_file).split('.')[0]
-                fname = os.path.join(
-                    temp_dir, str(shard_index), f'{cleaned_event_file}_{batch_index}.parquet'
-                )
+                verify_shard(shard, event_file)
+                cleaned_event_file = os.path.basename(event_file).split(".")[0]
+                fname = os.path.join(temp_dir, str(shard_index), f"{cleaned_event_file}_{batch_index}.parquet")
                 shard.write_parquet(fname, compression="uncompressed")
 
 
-def convert_flat_to_meds(source_flat_path: str, target_meds_path: str, num_shards: int = 100, num_proc: int = 1, time_formats: Iterable[str] = ("%Y-%m-%d %H:%M:%S%.f", "%Y-%m-%d")) -> None:
+def convert_flat_to_meds(
+    source_flat_path: str,
+    target_meds_path: str,
+    num_shards: int = 100,
+    num_proc: int = 1,
+    time_formats: Iterable[str] = ("%Y-%m-%d %H:%M:%S%.f", "%Y-%m-%d"),
+) -> None:
     if not os.path.exists(source_flat_path):
         raise ValueError(f'The source MEDS Flat folder ("{source_flat_path}") does not seem to exist?')
 
@@ -232,16 +286,16 @@ def convert_flat_to_meds(source_flat_path: str, target_meds_path: str, num_shard
     for shard_index in range(num_shards):
         os.mkdir(os.path.join(temp_dir, str(shard_index)))
 
-    shutil.copyfile(os.path.join(source_flat_path, 'metadata.json'), os.path.join(target_meds_path, 'metadata.json'))
+    shutil.copyfile(os.path.join(source_flat_path, "metadata.json"), os.path.join(target_meds_path, "metadata.json"))
 
     csv_tasks = []
     parquet_tasks = []
 
-    source_flat_data_path = os.path.join(source_flat_path, 'flat_data')
+    source_flat_data_path = os.path.join(source_flat_path, "flat_data")
 
     for flat_file in os.listdir(source_flat_data_path):
         full_flat_file = os.path.join(source_flat_data_path, flat_file)
-        if full_flat_file.endswith('.csv') or full_flat_file.endswith('.csv.gz'):
+        if full_flat_file.endswith(".csv") or full_flat_file.endswith(".csv.gz"):
             csv_tasks.append(full_flat_file)
         else:
             parquet_tasks.append(full_flat_file)
@@ -250,17 +304,59 @@ def convert_flat_to_meds(source_flat_path: str, target_meds_path: str, num_shard
     random.shuffle(csv_tasks)
     random.shuffle(parquet_tasks)
 
-    csv_processor = functools.partial(process_csv_file, decompressed_dir=decompressed_dir, temp_dir=temp_dir, num_shards=num_shards, time_formats=time_formats)
+    if num_proc != 1:
+        with mp.get_context("spawn").Pool(num_proc) as pool:
+            metadata_columns_set = set()
+            for columns in pool.imap_unordered(get_csv_columns, csv_tasks):
+                metadata_columns_set |= columns
+            for columns in pool.imap_unordered(get_parquet_columns, parquet_tasks):
+                metadata_columns_set |= columns
 
-    with multiprocessing.get_context("spawn").Pool(num_proc) as pool:
-        for _ in pool.imap_unordered(csv_processor, csv_tasks):
-            pass
-    
-    parquet_processor = functools.partial(process_parquet_file, temp_dir=temp_dir, num_shards=num_shards)
+            metadata_columns_set -= {"patient_id", "numeric_value", "datetime_value", "text_value", "time", "code"}
+            metadata_columns = sorted(list(metadata_columns_set))
 
-    with multiprocessing.get_context("spawn").Pool(num_proc) as pool:
-        for _ in pool.imap_unordered(parquet_processor, parquet_tasks):
-            pass
+            csv_processor = functools.partial(
+                process_csv_file,
+                decompressed_dir=decompressed_dir,
+                temp_dir=temp_dir,
+                num_shards=num_shards,
+                time_formats=time_formats,
+                metadata_columns=metadata_columns,
+            )
+            parquet_processor = functools.partial(
+                process_parquet_file, temp_dir=temp_dir, num_shards=num_shards, metadata_columns=metadata_columns
+            )
+
+            for _ in pool.imap_unordered(csv_processor, csv_tasks):
+                pass
+            for _ in pool.imap_unordered(parquet_processor, parquet_tasks):
+                pass
+    else:
+        metadata_columns_set = set()
+        for task in csv_tasks:
+            metadata_columns_set |= get_csv_columns(task)
+        for task in parquet_tasks:
+            metadata_columns_set |= get_parquet_columns(task)
+
+        metadata_columns_set -= {"patient_id", "numeric_value", "datetime_value", "text_value", "time", "code"}
+        metadata_columns = sorted(list(metadata_columns_set))
+
+        csv_processor = functools.partial(
+            process_csv_file,
+            decompressed_dir=decompressed_dir,
+            temp_dir=temp_dir,
+            num_shards=num_shards,
+            time_formats=time_formats,
+            metadata_columns=metadata_columns,
+        )
+        parquet_processor = functools.partial(
+            process_parquet_file, temp_dir=temp_dir, num_shards=num_shards, metadata_columns=metadata_columns
+        )
+
+        for task in csv_tasks:
+            csv_processor(task)
+        for task in parquet_tasks:
+            parquet_processor(task)
 
     shutil.rmtree(decompressed_dir)
 
@@ -273,17 +369,12 @@ def convert_flat_to_meds(source_flat_path: str, target_meds_path: str, num_shard
         print("Processing shard ", shard_index)
         shard_dir = os.path.join(temp_dir, str(shard_index))
 
+        if len(os.listdir(shard_dir)) == 0:
+            continue
+
         events = [pl.scan_parquet(os.path.join(shard_dir, a)) for a in os.listdir(shard_dir)]
 
-        all_events = pl.concat(events, how="diagonal_relaxed")
-
-        for important_column in ("patient_id", "time", "code"):
-            rows_with_invalid_code = all_events.filter(pl.col(important_column).is_null()).collect()
-            if len(rows_with_invalid_code) != 0:
-                print("Have rows with invalid " + important_column)
-                for row in rows_with_invalid_code:
-                    print(row)
-                raise ValueError("Cannot have rows with invalid " + important_column)
+        all_events = pl.concat(events)
 
         measurement = pl.struct(
             code=pl.col("code"),
@@ -293,14 +384,14 @@ def convert_flat_to_meds(source_flat_path: str, target_meds_path: str, num_shard
             metadata=pl.col("metadata"),
         )
 
-        grouped_by_time = all_events.groupby("patient_id", "time").agg(measurements=measurement)
+        grouped_by_time = all_events.group_by("patient_id", "time").agg(measurements=measurement)
 
         event = pl.struct(
             pl.col("time"),
             pl.col("measurements"),
         )
 
-        grouped_by_patient = grouped_by_time.groupby("patient_id").agg(events=event.sort_by(pl.col("time")))
+        grouped_by_patient = grouped_by_time.group_by("patient_id").agg(events=event.sort_by(pl.col("time")))
 
         # We now have our data in the final form, grouped_by_patient, but we have to do one final transformation
         # We have to convert from polar's large_list to list because large_list is not supported by huggingface
@@ -311,10 +402,10 @@ def convert_flat_to_meds(source_flat_path: str, target_meds_path: str, num_shard
         converted = grouped_by_patient.collect().to_arrow()
 
         # Now we need to reconstruct the schema
-        # We do this by pulling the metadata schema and then using meds.patient_schema
-        event_schema = converted.schema.field("events").type.value_type
-        measurement_schema = event_schema.field("measurements").type.value_type
-        metadata_schema = measurement_schema.field("metadata").type
+        if len(metadata_columns) == 0:
+            metadata_schema = pa.null()
+        else:
+            metadata_schema = pa.struct([(column, pa.string()) for column in metadata_columns])
 
         desired_schema = meds.patient_schema(metadata_schema)
 
@@ -325,6 +416,7 @@ def convert_flat_to_meds(source_flat_path: str, target_meds_path: str, num_shard
 
     shutil.rmtree(temp_dir)
 
+
 def convert_flat_to_meds_main():
     parser = argparse.ArgumentParser(prog="meds_etl_flat", description="Performs an ETL from MEDS Flat to MEDS")
     parser.add_argument("source_flat_path", type=str)
@@ -333,4 +425,3 @@ def convert_flat_to_meds_main():
     parser.add_argument("--num_proc", type=int, default=1)
     args = parser.parse_args()
     convert_flat_to_meds(**args)
-
