c981a707333a94bbaa1d5bba7bd12d01f178b206
Mcelfresh <s0374478@lap31192m.enterprise.stanfordmed.org>
2024-01-10 13:18:36 -0800

    start working with databricks

---
+++ src/meds_etl/tests/shc/test_flat_etl.py
@@ -0,0 +1,7 @@
+# note: for pytest, all test files containing tests, and all test function names must start with "test"
+
+def test_dummy_fail():
+    assert 1 == 0
+
+def test_dummy_pass():
+    assert 1 == 1
